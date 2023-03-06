ed8371406a194eda4e4f13777a91418b0c4d8c1e
Даниил Ермолаев <jude@MacBook-Pro-Daniil.local>
2023-03-06 23:29:33 +0500

    new posts requests

--- src/routers/admin.py
+++ src/routers/admin.py
@@ -16,15 +16,39 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+# ==== TEACHERS ====>
 @router.post('/add-teacher')
 async def add_teacher(teacher: Teacher, ) -> dict:
     await crud.add_teacher(teacher)
     return responses.OK_RESPONSES['simple']
 
-@router.post('/add-group')
-async def add_group() -> dict:
+@router.post('/edit-teacher')
+async def edit_teacher(teacher: Teacher, teacher_id: int) -> dict:
     return {'Hello': 'World!'}
 
-@router.post('/add-shedule')
-async def add_shedule() -> dict:
+@router.post('/delete-teacher')
+async def delete_teacher(teacher_id: int) -> dict:
     return {'Hello': 'World!'}
+
+# ==== GROUPS ====>
+@router.post('/add-group')
+async def add_group(group: Group) -> dict:
+    return {'Hello': 'World!'}
+
+@router.post('/edit-group')
+async def edit_group(group_id: int, group: Group) -> dict:
+    return {'Hello': 'World!'}
+
+@router.post('/remove-group')
+async def remove_group(group_id: int) -> dict:
+    return {'Hello': 'World!'}
+
+# ==== SHEDULE ====>
+@router.post('/add-shedule')
+async def add_shedule(group_id: int, shedule: Shedule) -> dict:
+    return {'Hello': 'World!'}
+
+@router.post('/edit-shedule')
+async def edit_shedule(group_id: int, shedule: Shedule) -> dict:
+    return {'Hello': 'World!'}
+
