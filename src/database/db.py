4dbdc00d06ec2fe3cfa82c8cf9c9a66a694bb7f7
Daniil Ermolaev <blcklptn@icloud.com>
2023-03-14 17:01:50 +0500

    rename host from localhost to db

--- src/database/db.py
+++ src/database/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from core.config import db_settings
 
-DATABASE_URL: str = f'postgresql://{db_settings.DB_USERNAME}:{db_settings.DB_PASSWORD}@localhost/{db_settings.DB_NAME}'
+DATABASE_URL: str = f'postgresql://{db_settings.DB_USERNAME}:{db_settings.DB_PASSWORD}@db/{db_settings.DB_NAME}'
 engine: object = create_engine(DATABASE_URL)
 session_local: object = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base: object = declarative_base()
